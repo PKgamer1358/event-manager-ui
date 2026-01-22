e5d7cf6bdefb976a6dc27c2df5d4178c92fe43ac
PKgamer1358 <prajjithprajwal@gmail.com>
2026-01-22 12:30:52 +0530

    Update source code

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,9 +5,8 @@ import { getStoredToken } from "./auth";
 export const API_BASE_URL = process.env.REACT_APP_API_URL
   ? process.env.REACT_APP_API_URL
   : Capacitor.isNativePlatform()
-  // ? "http://10.0.2.2:8000" // ANDROID EMULATOR
-  ? "http://10.89.36.187:8000" // REAL DEVICE (same Wi-Fi)
-  : "http://127.0.0.1:8000";
+    ? "https://event-manager-api-zqji.onrender.com" // Mobile use Render Prod
+    : "http://127.0.0.1:8000";
 
 console.log("API BASE URL:", API_BASE_URL);
 
